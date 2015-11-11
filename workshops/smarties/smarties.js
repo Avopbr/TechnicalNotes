38d2dec8f31d3680ef21109f7256eaeb38254213
Avopbr <abadwanguzeli@gmail.com>
2015-11-11 15:52:41 +0200

    Done fisrt three functions and passing tests

--- workshops/smarties/smarties.js
+++ workshops/smarties/smarties.js
@@ -1,15 +1,44 @@
-var findSmartiesByColor = function(){
+var findSmartiesByColor = function(smarties, color){
+	
+	 for (var smartyIndex in smarties) {
+		smarty = smarties[smartyIndex].color
+
+		if(smarty == color){
+			results = smarties[smartyIndex];
+
+		}
+	}
+	return 	results;
 
 };
+
 
 var findMostAbundantSmartie = function(smarties){
+	var max_smarty = {color:'', qty:0 }
 
+	for(var x in smarties) {
+		var currentSmarty = smarties[x];
+		if (currentSmarty.qty > max_smarty.qty) {
+			max_smarty = currentSmarty;
+				
+		}
+	}
+	return  max_smarty;
 };
 
-var findSmartiesLessThan = function(){
 
+var findSmartiesLessThan = function(smarties, num){
+	var smartieList = [];
+
+	for(var x in smarties) {
+		var smarty = smarties[x];
+		if (smarty.qty < num){
+			smartieList.push(smarty.qty)
+		}
+	}
+	return smartieList
 };
-
+/*
 var findSmarties = function(){
 
 };
@@ -25,3 +54,4 @@ var sortSmarties = function(){
 var addTotalForTeams = function(){
 
 };
+*/
